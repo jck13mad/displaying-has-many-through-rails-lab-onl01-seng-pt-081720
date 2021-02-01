4af0e0f6cece49db0f189e7af88290e65339147a
Jack Madden <jck13mad@gmail.com>
2021-02-01 00:37:05 +0000

    Automatically backed up by Learn

--- db/migrate/20210201003351_add_belongs_to_to_appointments.rb
+++ db/migrate/20210201003351_add_belongs_to_to_appointments.rb
@@ -1,6 +1,6 @@
 class AddBelongsToToAppointments < ActiveRecord::Migration[5.0]
   def change
     add_column :appointments, :patient, :belongs_to, index: true, foreign_key: true
-    add_column :appointments, :doctors
+    add_column :appointments, :doctor, :belongs_to, index: true, foreign_key: true 
   end
 end
