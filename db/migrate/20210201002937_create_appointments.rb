b24645cc9832e9dc2ef81a03553d179aa2bc97a4
Jack Madden <jck13mad@gmail.com>
2021-02-01 00:39:41 +0000

    Automatically backed up by Learn

--- db/migrate/20210201002937_create_appointments.rb
+++ db/migrate/20210201002937_create_appointments.rb
@@ -2,7 +2,7 @@ class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
-      t.
+      t.belongs_to :patient 
 
       t.timestamps
     end
