18619e013bf5d8062e80dfc406b178f47af0f634
Jack Madden <jck13mad@gmail.com>
2021-02-01 00:29:00 +0000

    Automatically backed up by Learn

--- db/migrate/20210201002723_create_appointments.rb
+++
@@ -1,9 +0,0 @@
-class CreateAppointments < ActiveRecord::Migration[5.0]
-  def change
-    create_table :appointments do |t|
-      t.datetime :appointment_datetime
-
-      t.timestamps
-    end
-  end
-end
