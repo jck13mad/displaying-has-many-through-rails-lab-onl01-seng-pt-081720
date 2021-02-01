6d036f7e7809825c14c514deac504005e4544113
Jack Madden <jck13mad@gmail.com>
2021-02-01 00:38:57 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210201003102) do
+ActiveRecord::Schema.define(version: 20210201002937) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
-  end
-
-  create_table "doctors", force: :cascade do |t|
-    t.string   "name"
-    t.string   "department"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
